a24591ecd420be3c6e57db2d3c9123cdfe436191
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2021-03-06 21:47:18 +0100

    feat: adjust base topic

--- src/main/java/com/reloadly/paypro/accountservice/constant/EventTopicConstant.java
+++ src/main/java/com/reloadly/paypro/accountservice/constant/EventTopicConstant.java
@@ -2,7 +2,7 @@ package com.reloadly.paypro.accountservice.constant;
 
 public class EventTopicConstant {
 
-    private static final String BASE_TOPIC = "fvsqlil0-";
+    private static final String BASE_TOPIC = "com.reloadly.paypro.event.";
 
     public static final String USER_CREATION = BASE_TOPIC + "user-creation";
 
